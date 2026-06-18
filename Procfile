64355cbe4a3e13ea535c1b07cc19fb7e595e1e2c
SuperFeifan <superfeifan@users.noreply.github.com>
2026-06-18 05:11:16 +0000

    Use Node.js buildpack with static server

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bin/start-nginx-single
+web: node server.js
---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const MIME = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
+};
+
+const server = http.createServer((req, res) => {
+  let filePath = req.url === '/' ? '/index.html' : req.url;
+  filePath = path.join(__dirname, filePath);
+
+  const ext = path.extname(filePath);
+  const contentType = MIME[ext] || 'text/plain';
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      fs.readFile(path.join(__dirname, 'index.html'), (e2, d2) => {
+        if (e2) {
+          res.writeHead(500);
+          return res.end('500');
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(d2);
+      });
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Shibuya Todo running on port ${PORT}`);
+});
